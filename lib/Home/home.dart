e49c146b4b191966d7a9025b853d56d679bc05aa
Omar Khaled <omarkhaled6154@gmail.com>
2022-10-29 01:31:32 +0200

    Version 0.1

---
+++ lib/Home/home.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              child: Text(
+                "Hello World!"
+            ))
+        ]),
+      )
+    );
+  }
+}
